9e821a6ae8950bc793bbb31da7ff6809d886e9ae
AraVraelHalt <ma8540@rit.edu>
2026-02-17 08:09:50 -0500

    Added Basic Login

---
+++ App/electron/main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: '#121212'
+  });
+
+  win.loadFile('login.html');
+}
+
+app.whenReady().then(createWindow);
+
