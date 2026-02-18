686740b18e9699d9d80b08090360de82e087a1ab
AraVraelHalt <ma8540@rit.edu>
2026-02-18 03:16:44 -0500

    dynamic change

--- App/python/client.py
+++ App/python/client.py
@@ -1,11 +1,14 @@
+from pathlib import Path
 import socks
 import socket
+
+addr = Path("../onion/hidden_service/hostname").read_text().strip()
 
 s = socks.socksocket()
 s.set_proxy(socks.SOCKS5, "127.0.0.1", 9050)
 
 try:
-    s.connect(("iklo4xrurtstonzpjujiyai2ikggau7xd4sx762v7mowlggbu5hqnfad.onion", 80))
+    s.connect((addr, 80))
     s.sendall(b"Hello Tor Server")
     data = s.recv(1024)
     print("Received:", data.decode())
