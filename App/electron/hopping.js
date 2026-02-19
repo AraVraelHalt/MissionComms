1459d45f8c6ccf6fb97629c341359de5457ee894
AraVraelHalt <ma8540@rit.edu>
2026-02-19 01:33:40 -0500

    Loading Tor

---
+++ App/electron/hopping.js
@@ -0,0 +1,24 @@
+let hoppingInterval;
+
+window.electronAPI.onPythonOutput((event, data) => {
+  console.log(data); // for debugging
+});
+
+window.electronAPI.onTorDone(() => {
+  clearInterval(hoppingInterval);
+  document.getElementById('status').innerText = "Tor Done";
+});
+
+function startHopping() {
+  hoppingInterval = setInterval(() => {
+    const randomIP = Array(4)
+      .fill(0)
+      .map(() => Math.floor(Math.random() * 256))
+      .join('.');
+    const statusEl = document.getElementById('status');
+    statusEl.innerText += `Hopping to ${randomIP}\n`;
+  }, 200);
+}
+
+startHopping();
+
