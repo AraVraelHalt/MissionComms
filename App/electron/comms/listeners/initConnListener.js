f05e579745926f60e9e005887c15c9f27551e170
AraVraelHalt <ma8540@rit.edu>
2026-02-26 01:56:29 -0500

    Modularized some code

---
+++ App/electron/comms/listeners/initConnListener.js
@@ -0,0 +1,50 @@
+const { ipcMain } = require('electron');
+const net = require('net');
+
+/**
+* Registers IPC handler for connecting to peer
+*
+* @param {BrowserWindow}
+*/
+function registerInitConnListener(win) {
+  ipcMain.handle('conn-to-peer', async (event, userInput) => {
+    return new Promise((resolve) => {
+      const client = net.createConnection({ host: '127.0.0.1', port: 6000 }, () => {
+        client.write(`JSCLIENTv1|${userInput}`);
+      });
+
+      let finished = false;
+
+      const cleanup = () => {
+        if (!finished) {
+          finished = true;
+          client.destroy();
+        }
+      };
+
+      client.on('data', (data) => {
+        if (finished) return;
+
+        const response = data.toString().trim();
+        cleanup();
+
+        if (response === 'VALID') {
+          win.loadFile('electron/loading/loading.html');
+          resolve('OK');
+        } else {
+          resolve('INVALID');
+        }
+      });
+
+      client.on('error', (err) => {
+        cleanup();
+        resolve('INVALID');
+      });
+
+      client.on('end', cleanup);
+      client.on('close', cleanup);
+    });
+  });
+}
+
+module.exports = { registerInitConnListener };
--- App/electron/main.js
+++ App/electron/main.js
@@ -4,6 +4,7 @@ const path = require('path');
 const net = require('net');
 
 const { registerSecureNodeHandler } = require('./comms/handlers/secureNodeHandler');
+const { registerInitConnListener } = require('./comms/listeners/initConnListener');
 
 // ----------------------
 // Background Processes
@@ -88,52 +89,6 @@ function startServer(win) {
   });
 
   win.webContents.send('server-started');
-}
-
-// ----------------------
-// Event Listeners 
-// ----------------------
-function registerInitConnListener(win) {
-  ipcMain.handle('conn-to-peer', async (event, userInput) => {
-
-    return new Promise((resolve) => {
-      const client = net.createConnection({ host: '127.0.0.1', port: 6000 }, () => {
-        client.write(`JSCLIENTv1|${userInput}`);
-      });
-
-      let finished = false;
-
-      const cleanup = () => {
-        if (!finished) {
-          finished = true;
-          client.destroy();
-        }
-      };
-
-      client.on('data', (data) => {
-        if (finished) return;
-
-        const response = data.toString().trim();
-        cleanup();
-
-        if (response === 'VALID') {
-          win.loadFile('electron/loading/loading.html');
-          resolve('OK');
-        }
-        else {
-          resolve('INVALID');
-        }
-      });
-
-      client.on('error', (err) => {
-          cleanup();
-          resolve('INVALID');
-      });
-
-      client.on('end', cleanup);
-      client.on('close', cleanup);
-    });
-  });
 }
 
 // ----------------------
